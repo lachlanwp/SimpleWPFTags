8c92cb6ddeefec7b7fcab70f83607de30feb5c7c
Lachlan <lachlan@chronosheets.com>
2017-04-27 19:43:21 +1000

    Initial Commit

---
+++ IndividualTag.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TagsControl
+{
+    /// <summary>
+    /// Interaction logic for IndividualTag.xaml
+    /// </summary>
+    public partial class IndividualTag : UserControl
+    {
+        public event EventHandler OnDeleteMyself;
+
+        public String TagStringValue
+        {
+          get { return (String)this.GetValue(TagStringValueProperty); }
+          set { this.SetValue(TagStringValueProperty, value); } 
+        }
+        public static readonly DependencyProperty TagStringValueProperty = DependencyProperty.Register(
+          "TagStringValue", typeof(String), typeof(IndividualTag),new PropertyMetadata(""));
+
+        public IndividualTag()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteTagBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(OnDeleteMyself != null)
+            {
+                OnDeleteMyself(this, e);
+            }
+        }
+    }
+}
---
+++ TagsControlRoot.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TagsControl
+{
+    /// <summary>
+    /// Interaction logic for TagsControl.xaml
+    /// </summary>
+    public partial class TagsControlRoot : UserControl
+    {
+
+        public TagsControlRoot()
+        {
+            InitializeComponent();
+        }
+
+        private void NewTagTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                //add the tag
+                IndividualTag newTag = new IndividualTag();
+                newTag.TagStringValue = NewTagTextBox.Text;
+                newTag.OnDeleteMyself += NewTag_OnDeleteMyself;
+
+                //1 behind text box
+                TagsContainer.Children.Insert(TagsContainer.Children.Count - 1, newTag);
+                
+                NewTagTextBox.Text = "";
+            }
+        }
+
+        private void NewTag_OnDeleteMyself(object sender, EventArgs e)
+        {
+            if(sender!=null)
+            { 
+                TagsContainer.Children.Remove((UIElement)sender);
+            }
+        }
+    }
+}
